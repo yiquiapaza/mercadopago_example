8d6580a587f882248ca100cd5ee49e016de84d94
Yhonatan Jesus <yiquiapaza@gmail.com>
2022-02-10 17:15:12 -0300

    Create a plan with API mercadopago

--- index.mjs
+++ index.mjs
@@ -1,5 +1,6 @@
+import 'dotenv/config'
 import express from "express";
-//import axios from "axios";
+import axios from "axios";
 
 const app = express();
 
@@ -13,17 +14,29 @@ app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
+
+// eslint-disable-next-line no-undef
+let plan = {}
+
 // [ ] Create a plan
 // [ ] Use axios to create a plam
 app.post('/create_plan', (req, res) => {
-    let obj = req.body;
-    console.log(obj);
-    res.type('application/json');
+    console.log(req);
+    plan = req.body;
+    axios.post(process.env.API_CREATE_PLAN,plan,{
+        headers: {
+            Authorization: `Bearer  ${process.env.ACCESS_TOKEN}`
+        }
+    }).then(response => {
+         console.log(response);
+    }).catch(error => {
+        console.log(error)
+    });
     res.status(200).send('Success');
 });
 
 // [ ] List of plans that were registered
- 
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
