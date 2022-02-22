879fcfe9b46cd719df0a9c06c38ba7ec7de5fdc3
Yhonatan Jesus <yiquiapaza@gmail.com>
2022-02-22 20:28:34 -0300

    Create users

--- index.mjs
+++ index.mjs
@@ -18,24 +18,46 @@ app.get('/', (req, res) => {
 // eslint-disable-next-line no-undef
 let plan = {}
 
-// [ ] Create a plan
-// [ ] Use axios to create a plam
+// eslint-disable-next-line no-undef
+let user = []
+
+// [x] Create a plan
+// [x] Use axios to create a plam
 app.post('/create_plan', (req, res) => {
-    console.log(req);
+    //console.log(req);
     plan = req.body;
-    axios.post(process.env.API_CREATE_PLAN,plan,{
+    axios.post(process.env.API_CREATE_PLAN, plan,{
         headers: {
-            Authorization: `Bearer  ${process.env.ACCESS_TOKEN}`
+            Authorization: `Bearer  ${process.env.ACCESS_TOKEN}`,
         }
     }).then(response => {
          console.log(response);
     }).catch(error => {
         console.log(error)
     });
+    console.log(user);
     res.status(200).send('Success');
 });
 
 // [ ] List of plans that were registered
+app.get('/list_plans');
+
+// [ ] Create Usear and save in a file
+app.post('/create_user', (req, res) => {
+    let data = req.body
+    axios.post(process.env.API_CREATE_USER, data, {
+        headers: {
+            Authorization:`Bearer ${process.env.ACCESS_TOKEN}` 
+        }
+    }).then(response => {    
+        user.push(req.body);
+        console.log(response);
+        res.status(200).send('Success');
+    }).catch(error => {
+        console.log(error);
+        res.status(400).send(error); 
+    });
+})
 
 const PORT = 3000;
 
