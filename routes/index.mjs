a67bf04a25298e09e9c44c782b6d9782e3791f64
Yhonatan Jesus <yiquiapaza@gmail.com>
2022-02-27 22:16:32 -0300

    Add pages

--- routes/index.mjs
+++ routes/index.mjs
@@ -3,7 +3,19 @@ import express from "express";
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index');
+});
+
+router.get('/users', (req, res) => {
+    res.render('users');
+});
+
+router.get('/plans', (req, res) => {
+    res.render('plans');
+});
+
+router.get('/cards', (req, res) => {
+    res.render('cards');
 });
 
 export default router;
